e70767e00d5a819a8de6ed66b14252d95e38d1e2
Helgrindi <v.decoen@gmail.com>
2025-02-20 18:57:03 +0100

    Added all strings to be used in the generator in three different variables: start, middle & end.

---
+++ script.js
@@ -0,0 +1,44 @@
+// Random message generator - conspiracy theory edition
+
+// Different parts of the messages to be used:
+const start = [
+    "They don't want you to know this, but",
+    "Wake up, sheeple! The truth is",
+    "Hidden in plain sight is the fact that",
+    "Ever wondered why nobody talks about how",
+    "The government has been covering this up for years",
+    "It's all connected: the pyramids, the moon, and",
+    "What if I told you",
+    "The so-called 'scientists' don't want you asking questions about",
+    "The truth is out there, and it's worse than you think",
+    "History books erased the fact that"
+];
+const middle = [
+    "Pigeons are actually government surveillance drones.",
+    "The moon is just a giant screensaver controlled by NASA.",
+    "All coffee shops are secretly collectin fingerprints for the Illuminati.",
+    "Lizard people run all major fast food chains.",
+    "The internet was actually invented in ancient Egypt.",
+    "Your phone listens to you even when it's turned off, especially when it's off.",
+    "All pineapples used to be carnivorous until they were genetically modified.",
+    "Cheese was originally an alien technology before it was stolen by humans.",
+    "Octopuses are proof that aliens exist, they have three hearts and blue blood!",
+    "The number 7 doesn't actually exist, and we just pretend it does.",
+    "WiFi signals are actually a government mind-control experiment.",
+    "Sharks don't attack people randomly, they are programmed to take out specific targets.",
+    "Airplane mode doesn't actually turn off anything, it just makes you feel safe.",
+    "Every time you blink, the simulation resets a little bit.",
+    "The dinosaurs never went extinct, they just moved underground."
+];
+const end = [
+    "Wake up, sheeple!",
+    "The truth is out there, but they don't want you to see it.",
+    "Do your own research before it's too late!",
+    "Start questioning everything... except this message.",
+    "Spread the word before they take this down!",
+    "Trust no one. Especially your toaster.",
+    "Once you know this, there's no turning back.",
+    "They are already watching you, act normal.",
+    "Protect yourself with aluminium foil. Lots of it.",
+    "Only those who can see the patterns will survive."
+];
